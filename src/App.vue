4a145127b5717fefa4fca3b298b3e82501f3c99b
Karlo Dela Rosa <jrosa@salespond.com>
2023-05-11 08:02:26 +0800

    Add water ripple

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
+import { ref, computed, reactive } from 'vue'
 import { RouterView } from 'vue-router'
 import NavMenu from './components/molecule/NavMenu.vue'
 import FooterMenu from './components/molecule/FooterMenu.vue'
+import { useMouseInElement } from '@vueuse/core'
+
+let randomDrop = ($el: any) => {
+  var x = Math.random() * $el.outerWidth();
+  var y = Math.random() * $el.outerHeight();
+  var dropRadius = 20;
+  var strength = 0.04 + Math.random() * 0.04;
+  $el.ripples('drop', x, y, dropRadius, strength);
+}
+
+let setRandomInterval = (callback: any, minInterval: any, maxInterval: any) => {
+  setTimeout(() => {
+    callback();
+    setRandomInterval(callback, minInterval, maxInterval);      
+  }, Math.floor(Math.random() * (maxInterval - minInterval + 1) + minInterval));
+}
 
 $(document).ready(() => {
-  console.info('jq')
-  $('.hero-banner').ripples({
+  let $el = $('.hero-banner').ripples({
     resolution: 800,
     dropRadius: 20,
     perturbance: 0.04,
   });
+
+  setRandomInterval(() => randomDrop($el), 500, 3000);
 })
 </script>
 
